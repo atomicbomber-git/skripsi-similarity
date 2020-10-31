a377e1cda305adc38b27ab29aa6b1a3f3fbc1907
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-10-31 21:37:34 +0700

    Updates.

--- app/Http/Controllers/MahasiswaDashboardController.php
+++ app/Http/Controllers/MahasiswaDashboardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SkripsiFingerprintHash;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\ResponseFactory;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaDashboardController extends Controller
 {
@@ -25,8 +27,50 @@ class MahasiswaDashboardController extends Controller
     {
         $mahasiswa->load("skripsi");
 
+        $userSkripsiHashesQuery =
+            SkripsiFingerprintHash::query()
+                ->select("hash_b.hash")
+                ->from(DB::raw("skripsi_fingerprint_hash AS hash_b"))
+                ->join(DB::raw("skripsi skripsi_b"), "skripsi_b.id", "=", "hash_b.skripsi_id")
+                ->where("skripsi_b.user_id", "=", $mahasiswa->id);
+
+        $mahasiswas = User::query()
+            ->select("users.id", "users.name", "skripsi.judul AS judul")
+            ->where("level", User::LEVEL_MAHASISWA)
+            ->where("users.id", "<>", $mahasiswa->id)
+            ->leftJoin("skripsi", "skripsi.user_id", "=", "users.id")
+            ->addSelect([
+                "similarity" => SkripsiFingerprintHash::query()
+                    ->from(DB::raw("skripsi_fingerprint_hash AS hash_a"))
+                    ->selectRaw("
+                2 * COUNT(DISTINCT hash_a.hash)  
+                /
+                (      
+                  (
+                      SELECT COUNT(DISTINCT sfh1.hash) FROM skripsi_fingerprint_hash sfh1
+                          JOIN skripsi s1 ON s1.id = sfh1.skripsi_id
+                          WHERE s1.user_id = ?
+                  ) +
+
+                  (
+                      SELECT COUNT(DISTINCT sfh2.hash) FROM skripsi_fingerprint_hash sfh2
+                          JOIN skripsi s2 ON s2.id = sfh2.skripsi_id
+                          WHERE s2.user_id = users.id            
+                  )
+                )
+            ", [$mahasiswa->id])
+                    ->whereColumn("hash_a.skripsi_id", "skripsi.id")
+                    ->whereIn(
+                        "hash_a.hash",
+                        $userSkripsiHashesQuery
+                    )
+            ])
+            ->orderByDesc("similarity")
+            ->paginate();
+
         return $this->responseFactory->view("mahasiswa.dashboard", [
             "mahasiswa" => $mahasiswa,
+            "mahasiswas" => $mahasiswas,
         ]);
     }
 }
--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -72,4 +72,44 @@
             @endif
         </div>
     </div>
+
+    <div>
+        @if($mahasiswas->isNotEmpty())
+            <div class="table-responsive">
+                <table class="table table-sm table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th> # </th>
+                        <th> Nama </th>
+                        <th> Skripsi </th>
+                        <th> Similaritas </th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach ($mahasiswas as $mahasiswa)
+                        <tr>
+                            <td> {{ $mahasiswas->firstItem() + $loop->index }} </td>
+                            <td> {{ $mahasiswa->name  }} </td>
+                            <td> {{ $mahasiswa->judul  }} </td>
+                            <td> {{ $mahasiswa->similarity * 100  }}% </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                {{ $mahasiswas->links() }}
+            </div>
+
+        @else
+            <div class="alert alert-warning">
+                <i class="fas fa-exclamation-triangle"></i>
+                {{ __("messages.errors.no_data") }}
+            </div>
+        @endif
+    </div>
+
+
 @endsection
