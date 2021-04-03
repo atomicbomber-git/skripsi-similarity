6aa80394ecc434a4872efee17fe825396ef77836
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-04-03 11:30:49 +0700

    Updates.

--- routes/web.php
+++ routes/web.php
@@ -30,74 +30,14 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get("/test/{mahasiswa}", function (User $mahasiswa) {
-    $targetSkripsi = Skripsi::query()
-        ->select("id", "judul", "user_id")
-        ->where("user_id", "=", $mahasiswa->getKey())
-        ->with([
-            "kalimatSkripsis:id,skripsi_id,teks",
-            "kalimatSkripsis.kalimatHashes:id,kalimat_skripsi_id,hash"
-        ])
-        ->first();
-
-    $otherSkripsis = Skripsi::query()
-        ->select("id", "judul", "user_id")
-        ->where("user_id", "<>", $mahasiswa->getKey())
-        ->with([
-            "kalimatSkripsis:id,skripsi_id,teks",
-            "kalimatSkripsis.kalimatHashes:id,kalimat_skripsi_id,hash"
-        ])
-        ->get();
-
-    return $otherSkripsis->map(function (Skripsi $otherSkripsi) use ($targetSkripsi) {
-        $kalimatSimilarities = collect();
-
-        foreach ($targetSkripsi->kalimatSkripsis as $kalimatA) {
-            foreach ($otherSkripsi->kalimatSkripsis as $kalimatB) {
-                $kalimatSimilarities->push(new KalimatSimilarityRecord(
-                    kalimatAId: $kalimatA->getKey(),
-                    kalimatBId: $kalimatB->getKey(),
-                    chebyshevDistance: $kalimatA->chebyshevDistanceFrom($kalimatB),
-                    diceSimilarity: $kalimatA->diceSimilarityWith($kalimatB),
-                ));
-            }
-        }
-
-        $maxChebyshev = $kalimatSimilarities->max("chebyshevDistance");
-
-        return new SkripsiSimilarityRecord([
-            "skripsi" => $otherSkripsi,
-            "mostSimilarKalimats" => $kalimatSimilarities
-                ->sortByDesc(fn (KalimatSimilarityRecord $data) => (($data->chebyshevDistance / $maxChebyshev) + $data->diceSimilarity) / 2)
-                ->take(5),
-            "chebyshevDistanceAverage" => $kalimatSimilarities->average("chebyshevDistance"),
-            "diceSimilarityAverage" => $kalimatSimilarities->average("diceSimilarity"),
-        ]);
-    });
-});
-
-
 Route::get('/', function () {
     return redirect()->route("login");
 });
 
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/mahasiswa', MahasiswaController::class);
-
-Route::get('bank-skripsi-mahasiswa', BankSkripsiMahasiswaController::class)
-    ->name('bank-skripsi-mahasiswa');
-
-Route::get('/mahasiswa/{mahasiswa}/dashboard', MahasiswaDashboardController::class)
-    ->name("mahasiswa.dashboard");
-
-Route::get('/mahasiswa/{mahasiswa}/download-skripsi', SkripsiFileDownloadController::class)
-    ->name("mahasiswa.download-skripsi");
-
-Route::post('/mahasiswa/{mahasiswa}/upload-skripsi', SkripsiFileUploadController::class)
-    ->name("mahasiswa.upload-skripsi");
-
-Route::delete('/mahasiswa/{mahasiswa}/delete-skripsi', SkripsiDeleteController::class)
-    ->name("mahasiswa.delete-skripsi");
-
-Route::view("/ViewerJS/{all?}", "ViewerJS.index")->name("ViewerJS");
+Route::get('bank-skripsi-mahasiswa', BankSkripsiMahasiswaController::class)->name('bank-skripsi-mahasiswa');
+Route::get('/mahasiswa/{mahasiswa}/dashboard', MahasiswaDashboardController::class)->name("mahasiswa.dashboard");
+Route::get('/mahasiswa/{mahasiswa}/download-skripsi', SkripsiFileDownloadController::class)->name("mahasiswa.download-skripsi");
+Route::post('/mahasiswa/{mahasiswa}/upload-skripsi', SkripsiFileUploadController::class)->name("mahasiswa.upload-skripsi");
+Route::delete('/mahasiswa/{mahasiswa}/delete-skripsi', SkripsiDeleteController::class)->name("mahasiswa.delete-skripsi");
