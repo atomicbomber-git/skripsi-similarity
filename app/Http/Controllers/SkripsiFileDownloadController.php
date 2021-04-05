7d576ed624e538fb0aa50d5be92bfd8951f9d416
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-04-05 18:18:56 +0700

    Updates.

--- app/Http/Controllers/SkripsiFileDownloadController.php
+++ app/Http/Controllers/SkripsiFileDownloadController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Constants\MessageState;
+use App\Helper\SessionHelper;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\ResponseFactory;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use ZipArchive;
 
 class SkripsiFileDownloadController extends Controller
 {
@@ -22,7 +26,7 @@ class SkripsiFileDownloadController extends Controller
      *
      * @param Request $request
      * @param User $mahasiswa
-     * @return Response|BinaryFileResponse
+     * @return RedirectResponse|Response|BinaryFileResponse
      */
     public function __invoke(Request $request, User $mahasiswa)
     {
@@ -30,8 +34,35 @@ class SkripsiFileDownloadController extends Controller
             return $this->responseFactory->noContent(404);
         }
 
+        $zip = new ZipArchive();
+        $tmpZipPath = database_path('temporary.zip');
+        if (file_exists($tmpZipPath)) {
+            unlink($tmpZipPath);
+        }
+
+        if ($zip->open($tmpZipPath,  ZipArchive::CREATE)) {
+            foreach ($mahasiswa->skripsi->media as $medium) {
+                $zip->addFile(
+                    $medium->getPath(),
+                    $medium->file_name
+                );
+            }
+            $zip->close();
+        } else {
+            SessionHelper::flashMessage(
+                "Gagal memroses berkas skripsi",
+                MessageState::STATE_DANGER,
+            );
+
+            unlink($tmpZipPath);
+            return redirect()->back();
+        }
+
         return $this->responseFactory->file(
-            $mahasiswa->skripsi->getFirstMediaPath(),
-        );
+            $tmpZipPath
+        , [
+            'Content-Disposition' => "attachment; filename={$mahasiswa->name} - ({$mahasiswa->skripsi->judul}).zip",
+            'Content-Type' => 'application/zip',
+        ]);
     }
 }
