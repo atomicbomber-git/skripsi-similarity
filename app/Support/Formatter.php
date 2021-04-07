9621caf309802634f164ce261fdd26362b7118e7
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-04-07 17:40:23 +0700

    Updates.

--- app/Support/Formatter.php
+++ app/Support/Formatter.php
@@ -13,6 +13,10 @@ class Formatter
 
     public static function percentage($value): string
     {
-        return is_null($value) ? "-" : number_format($value * 100, 4) . " %";
+        $formatted = number_format($value * 100, 2);
+        $formatted = rtrim($formatted, '0');
+        $formatted = rtrim($formatted, '.');
+
+        return is_null($value) ? "-" : $formatted . " %";
     }
 }
